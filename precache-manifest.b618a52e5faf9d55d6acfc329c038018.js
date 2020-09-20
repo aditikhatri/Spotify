064b9b608e342a6ff151e40ab48049f041463e60
aditi khatri <aditikhatri31@gmail.com>
2020-09-20 20:33:57 +0530

    Updates

---
+++ precache-manifest.b618a52e5faf9d55d6acfc329c038018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17acda68c76a5f3b2acc2f7dbbecb35c",
+    "url": "/Spotify/index.html"
+  },
+  {
+    "revision": "a2ed8e692ca2688c9a01",
+    "url": "/Spotify/static/css/main.a755b76e.chunk.css"
+  },
+  {
+    "revision": "c0bea8b9440e03eea0fd",
+    "url": "/Spotify/static/js/2.012265a9.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/Spotify/static/js/2.012265a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ed8e692ca2688c9a01",
+    "url": "/Spotify/static/js/main.f568a492.chunk.js"
+  },
+  {
+    "revision": "3d0e9640b4594ab9f83c",
+    "url": "/Spotify/static/js/runtime-main.829d69a8.js"
+  }
+]);
